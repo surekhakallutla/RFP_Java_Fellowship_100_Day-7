579f1c7b74baea4a8cf72a19ab34a815cf8cac45
surekhakallutla <surekhakallutla@gmail.com>
2022-01-19 10:54:59 +0530

    Whether Gambler Won or Lost Bet 50% of the Stake would resign for the Day

--- src/com/bridgelabz/gambling/GamblingSimulator.java
+++ src/com/bridgelabz/gambling/GamblingSimulator.java
@@ -13,23 +13,29 @@ public class GamblingSimulator {
 		int stake = 100;
 		Scanner scn = new Scanner(System.in);
 		Random rad = new Random();
-		int betresult = rad.nextInt(2);
+		
 	//	System.out.println("Betresult "+betresult);
 		int remainingstake;
-		
-		if(betresult == 1)
+		while(stake > 50)
 		{
-			System.out.println("The Gambler won the bet");
-			 remainingstake = stake + bet ;
-			 System.out.println("The Stake after Winning the bet is "+remainingstake);
-		}
-		else
-		{
-			System.out.println("The Gambler lost the bet");
-			 remainingstake = stake - bet;
-			 System.out.println("The Stake after Loosing the bet is "+remainingstake);
-		}
+			int betresult = rad.nextInt(2);
+			if(betresult == 1)
+			{
+				System.out.println("The Gambler won the bet");
+				 remainingstake = stake + bet ;
+				 stake = remainingstake;
+				 System.out.println("The Stake after Winning the bet is "+stake);
+			}
+			else
+			{
+				System.out.println("The Gambler lost the bet");
+				 remainingstake = stake - bet;
+				 stake = remainingstake;
+				 System.out.println("The Stake after Loosing the bet is "+stake);
+			}
 
+		}
+		
 	}
 
 }
