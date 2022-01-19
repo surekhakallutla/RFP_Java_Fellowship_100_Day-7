0675af88f12901504150b1b5d7df6c7ad269915a
surekhakallutla <surekhakallutla@gmail.com>
2022-01-19 10:42:40 +0530

    Checking Either the Gambler Won or Lost the bet

--- src/com/bridgelabz/gambling/GamblingSimulator.java
+++ src/com/bridgelabz/gambling/GamblingSimulator.java
@@ -1,5 +1,8 @@
 package com.bridgelabz.gambling;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class GamblingSimulator {
 
 	public static void main(String[] args) {
@@ -7,7 +10,25 @@ public class GamblingSimulator {
 		
 		final int startstake = 100;
 		final int bet = 1;
+		int stake = 100;
+		Scanner scn = new Scanner(System.in);
+		Random rad = new Random();
+		int betresult = rad.nextInt(2);
+	//	System.out.println("Betresult "+betresult);
+		int remainingstake;
 		
+		if(betresult == 1)
+		{
+			System.out.println("The Gambler won the bet");
+			 remainingstake = stake + bet ;
+			 System.out.println("The Stake after Winning the bet is "+remainingstake);
+		}
+		else
+		{
+			System.out.println("The Gambler lost the bet");
+			 remainingstake = stake - bet;
+			 System.out.println("The Stake after Loosing the bet is "+remainingstake);
+		}
 
 	}
 
