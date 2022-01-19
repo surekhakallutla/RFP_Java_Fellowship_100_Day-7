8e45d594e1e251ab2f0b654cbe4346a5f568502e
surekhakallutla <surekhakallutla@gmail.com>
2022-01-19 10:11:01 +0530

    Assigning Start Stake as 100$ and Bet 1$

--- src/com/bridgelabz/gambling/GamblingSimulator.java
+++ src/com/bridgelabz/gambling/GamblingSimulator.java
@@ -4,6 +4,10 @@ public class GamblingSimulator {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Simulator Program");
+		
+		final int startstake = 100;
+		final int bet = 1;
+		
 
 	}
 
