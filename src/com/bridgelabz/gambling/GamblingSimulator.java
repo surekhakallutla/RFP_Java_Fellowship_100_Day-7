0e9ed7bbd3e6faaeaa07d3f329ce66837004dd73
surekhakallutla <surekhakallutla@gmail.com>
2022-01-19 09:28:37 +0530

    Welcome to Gambling Simulator Program

---
+++ src/com/bridgelabz/gambling/GamblingSimulator.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.gambling;
+
+public class GamblingSimulator {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Gambling Simulator Program");
+
+	}
+
+}
